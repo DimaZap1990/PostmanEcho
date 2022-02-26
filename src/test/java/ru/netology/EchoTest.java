449b6036dc183aab089635fa35c910c4ad1d8b11
Dima Zap <zapilenovd@gmail.com>
2022-02-26 09:52:00 +0300

    Test passed

--- src/test/java/ru/netology/EchoTest.java
+++ src/test/java/ru/netology/EchoTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 class EchoTest {
     @Test
@@ -14,7 +15,7 @@ class EchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body()
+                .body("data", equalTo("some data"))
                 ;
 
 
